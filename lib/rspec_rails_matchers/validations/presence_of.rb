984906eff3ee6cde467be01555eb5c69a8e501f5
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-02-18 01:49:18 -0800

    Update presence_of to accept :on option to test contextual validation.

--- lib/rspec_rails_matchers/validations/presence_of.rb
+++ lib/rspec_rails_matchers/validations/presence_of.rb
@@ -1,17 +1,24 @@
 module RspecRailsMatchers
   module Validations
     module PresenceOf
-      def validate_presence_of(attribute)
+      def validate_presence_of(attribute, options = {})
+        context = options[:on]
         Rspec::Matchers::Matcher.new :validate_presence_of, attribute do |_attr_|
           match do |model|
             model.send("#{_attr_}=", nil)
-            model.invalid? && model.errors[_attr_].any?
+            model.invalid?(context) && model.errors[_attr_].any?
           end
 
           failure_message_for_should do |model|
-            RspecRailsMatchers::Message.error(
-              :expected => [ "%s to validate presence of %s", model, _attr_ ]
-            )
+            if context
+              RspecRailsMatchers::Message.error(
+                :expected => [ "%s to validate presence of %s on %s", model, _attr_, context ]
+              )
+            else
+              RspecRailsMatchers::Message.error(
+                :expected => [ "%s to validate presence of %s", model, _attr_ ]
+              )
+            end
           end
         end
       end
